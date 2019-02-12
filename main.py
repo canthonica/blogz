e21d681b9e89173370edcf76d33f959471853356
canthonica <c.anthony.ca354@gmail.com>
2019-02-11 22:40:33 -0600

    final step before case1

--- main.py
+++ main.py
@@ -8,33 +8,38 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(340))
     #completed = db.Column(db.Boolean)
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
         #self.completed = False
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
+    #if request.method == 'POST':
+        #task_name = request.form['task']
+        #new_task = Task(task_name)
+       # db.session.add(new_task)
+        #db.session.commit()
 
     #tasks = Task.query.filter_by(completed=False).all()
     #completed_tasks = Task.query.filter_by(completed=True).all()
     return render_template('blog_page.html', title="Build a Blog")
 
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+    return render_template('blog_page.html')
 
-@app.route('/add_blog', methods=['POST', 'GET'])
-def addblog ():
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost ():
 
     #task_id = int(request.form['task-id'])
     #task = Task.query.get(task_id)
