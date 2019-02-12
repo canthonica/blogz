a3e8771330795079d93f900c635a96c3f74a988d
canthonica <c.anthony.ca354@gmail.com>
2019-02-11 19:15:30 -0600

    restart of build a blog

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,122 +6,44 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = '12qwaszx34erdfcv'
 
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
-    #owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    def __init__(self, name,):    #took out ownner
-        self.name = name
-        self.completed = False
-        #self.owner = owner
-'''
 
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    #body = db.Column(db.String(340))
-    #tasks = db.relationship('Task', backref='owner')
+    name = db.Column(db.String(120))
+    #completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
-        #self.password = password
+        #self.completed = False
 
-@app.before_request
-def require_login():
-    allowed_routes = ['login', 'register']
-    if request.endpoint not in allowed_routes and 'email' not in session:
-        return redirect('/login')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
-            
-            session['email'] = email
-            flash("Logged in")
-           
-            return redirect('/')
-        else:
-            flash('User password incorrect or user does not exist', 'error')
-            
-    return render_template('login.html')
-
-@app.route('/register', methods=['POST', 'GET']) 
-def register():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        verify = request.form['verify']
-
-        # TODO - validateuser's data
-
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user = User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['email'] = email
-            
-            return redirect('/')
-        else:
-            # TODO - user better respons messaging
-            return "<h1>Duplicate user</h1>"
-
-    return render_template('register.html')
-
-@app.route('/logout')
-def logout():
-    del session['email']
-    return redirect('/')
-'''
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    #owner = User.query.filter_by(email=session['email']).first()
-    
+
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)# removed owner
+        new_task = Task(task_name)
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.all()
-    #tasks = Task.query.filter_by(completed=False).all() #removed owner
+    #tasks = Task.query.filter_by(completed=False).all()
     #completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('blog_page.html', title=" Build a Blog", tasks=tasks) #removed completed_tasks=completed_tasks
-@app.route('/add_blog', methods=['POST', 'GET'])
-def new_blog():
-    
-    
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)# removed owner
-        db.session.add(new_task) 
-        db.session.commit()
-    tasks = Task.query.all()
-    
-    return render_template('add_blog.html', title=" Add a Blog")
+    return render_template('blog_page.html', title="Build a Blog")
 
 
+@app.route('/add_blog', methods=['POST'])
+def addblog ():
 
-@app.route('/blog_page', methods=['POST'])
-def blogs():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
+    #task_id = int(request.form['task-id'])
+    #task = Task.query.get(task_id)
     task.completed = True
     db.session.add(task)
     db.session.commit()
-    return redirect('/')
+
+    return render_template('add_blog.html')
+
 
 if __name__ == '__main__':
-
     app.run()
