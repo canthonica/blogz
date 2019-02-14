60fe7b41e3a3c4ba1db70f9e36b59d8158c293ea
canthonica <c.anthony.ca354@gmail.com>
2019-02-14 10:54:07 -0600

    working on same page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,48 +6,50 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
+#app.secret_key = '12qwaszx34erdfcv'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(340))
-    #completed = db.Column(db.Boolean)
+    body = db.Column(db.String(240))
+    completed = db.Column(db.Boolean)
 
-    def __init__(self, title, body):
+    def __init__(self, title):
         self.title = title
-        self.body = body
-        #self.completed = False
-
+        #self.body = body
+        self.completed = False
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    #if request.method == 'POST':
-        #task_name = request.form['task']
-        #new_task = Task(task_name)
-       # db.session.add(new_task)
-        #db.session.commit()
+    if request.method == 'POST':
+        task_name = request.form['task']
+        new_task = Blog(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    #tasks = Task.query.filter_by(completed=False).all()
-    #completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('blog_page.html', title="Build a Blog")
-
-@app.route('/blog', methods=['POST', 'GET'])
-def blog():
-    return render_template('blog_page.html')
-
+    tasks = Blog.query.filter_by(completed=False).all()
+    completed_tasks = Blog.query.filter_by(completed=True).all()
+    return render_template('blog_page.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
+'''
 @app.route('/newpost', methods=['POST', 'GET'])
-def newpost ():
+def newpost():
+    
+    return render_template('add-blog.html')
+'''
 
-    #task_id = int(request.form['task-id'])
-    #task = Task.query.get(task_id)
-    #task.completed = True
-    #db.session.add(task)
-    #db.session.commit()
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
 
-    return render_template('add_blog.html')
+    task_id = int(request.form['task-id'])
+    task = Blog.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
